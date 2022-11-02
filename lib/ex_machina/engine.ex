ba81ec62a37d7191ef75b4082401fe378daa09fd
Flavio Auciello <flavioauciello@nebulab.com>
2022-11-02 20:30:35 +0100

    ExMachina.Engine

---
+++ lib/ex_machina/engine.ex
@@ -0,0 +1,50 @@
+defmodule ExMachina.Engine do
+  require Logger
+
+  alias ExMachina.{Tape, State, Program}
+
+  @moduledoc """
+  Turing Machine engine.
+
+  ## Example
+
+  iex> tape = ExMachina.Tape.create([1,2,3,4])
+  iex> state = 0
+  iex> step = {{1, 0}, {:first, 1, :right}}
+  iex> program = [step]
+  iex> {tape, state, _} = ExMachina.Engine.step({tape, state, program})
+  iex> ExMachina.Tape.get(tape)
+  2
+  iex> state
+  1
+  """
+
+  def step({:error, payload}), do: {:error, payload}
+  def step({tape, state, program}) do
+    with symbol <- Tape.get(tape),
+         {_, {out_symbol, out_state, mov}} <- find_step({symbol, state}, program) do
+      out_tape =
+        tape
+        |> Tape.put(out_symbol)
+        |> Tape.move(mov)
+
+      Logger.info("[#{inspect tape}]")
+      Logger.info("{#{inspect symbol}, #{inspect state}} -> {#{inspect out_symbol}, #{inspect out_state}, #{inspect mov}}")
+      Logger.info("[#{inspect out_tape}]\n")
+
+      {out_tape, out_state, program}
+    end
+  end
+
+  defp find_step(_, step) when is_tuple(step), do: step
+  defp find_step({symbol, state}, program) when is_list(program) do
+    case Enum.find(program, fn {input, _} -> eq_step?(input, {symbol, state}) end) do
+      step when is_tuple(step) -> step
+      _ -> {:error, {symbol, state}}
+    end
+  end
+
+  defp eq_step?({symbol1, state1}, {symbol2, state2}) do
+    (symbol1 == symbol2) && (state1 == state2)
+  end
+end
--- test/ex_machina_test.exs
+++ test/ex_machina_test.exs
@@ -4,4 +4,36 @@ defmodule ExMachinaTest do
   doctest ExMachina.Tape
   doctest ExMachina.State
   doctest ExMachina.Program
+  doctest ExMachina.Engine
+
+  setup :machine_fixture
+
+  test "executing a Turing machine program", %{machine: {tape, state, program}} do
+    {tape, state, _} = ExMachina.Engine.step({tape, state, program})
+    assert ExMachina.Tape.to_list(tape) == ~w[A b c d]
+    assert state == 0
+
+    {tape, state, _} = ExMachina.Engine.step({tape, state, program})
+    assert ExMachina.Tape.to_list(tape) == ~w[A B c d]
+    assert state == 0
+
+    {tape, state, _} = ExMachina.Engine.step({tape, state, program})
+    assert ExMachina.Tape.to_list(tape) == ~w[A B C d]
+    assert state == 0
+
+    {tape, state, _} = ExMachina.Engine.step({tape, state, program})
+    assert ExMachina.Tape.to_list(tape) == ~w[A B C D]
+    assert state == -1
+  end
+
+  defp machine_fixture(_) do
+    tape = ExMachina.Tape.create(["a", "b", "c", "d"])
+    program = [
+      {{"a", 0}, {"A", 0, :right}},
+      {{"b", 0}, {"B", 0, :right}},
+      {{"c", 0}, {"C", 0, :right}},
+      {{"d", 0}, {"D", -1, nil}},
+    ]
+    {:ok, machine: {tape, 0, program}}
+  end
 end
