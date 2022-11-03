8516d9ad3e70e461ad8990fc93ddfef99af682f2
Flavio Auciello <flavioauciello@nebulab.com>
2022-11-03 21:27:58 +0100

    ExMachina server

--- lib/ex_machina.ex
+++ lib/ex_machina.ex
@@ -3,16 +3,37 @@ defmodule ExMachina do
   Documentation for `ExMachina`.
   """
 
-  @doc """
-  Hello world.
+  use GenServer
 
-  ## Examples
+  alias ExMachina.{Tape, State, Program, Engine}
 
-      iex> ExMachina.hello()
-      :world
+  def start_link(tape, program) do
+    GenServer.start_link(__MODULE__, {tape, program})
+  end
 
-  """
-  def hello do
-    :world
+  def init({tape, program}) do
+    with tape <- Tape.create(tape), state <- 0 do
+      if Program.valid?(program) do
+        {:ok, {tape, state, program}}
+      else
+        {:error, :invalid_program}
+      end
+    end
+  end
+
+  def start(pid) do
+    GenServer.call(pid, :start)
+  end
+
+  def handle_call(:start, caller, machine) do
+    Process.send_after(self(), :start, 500)
+    {:reply, :ok, {machine, caller}}
+  end
+
+  def handle_info(:start, {machine, {caller, _ref}}) do
+    with {tape, _, _} = ExMachina.Engine.run(machine) do
+      send(caller, {:ok, Tape.to_list(tape)})
+    end
+    {:noreply, machine}
   end
 end
